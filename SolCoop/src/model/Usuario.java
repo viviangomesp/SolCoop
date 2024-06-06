b98a5652dc589da4ad58ef75aa9dbf9686bc01b5
Vivian Gomes <96631345+viviangomesp@users.noreply.github.com>
2024-06-06 19:13:33 -0300

    update sunshare
    ajustes
    craicao de main na classe filha

--- SolCoop/src/model/Usuario.java
+++ SolCoop/src/model/Usuario.java
@@ -56,4 +56,6 @@ public class Usuario {
         this.endereco = endereco;
     }
 
+
+    
 }
--- SolCoop/src/model/share/SunShare.java
+++ SolCoop/src/model/share/SunShare.java
@@ -4,9 +4,9 @@ import model.Usuario;
 import java.util.Scanner;
 
 public class SunShare extends Usuario {
-    private float energiaDisponivel;
-    private float energiaCompartilhada;
-    private float energiaTotal;
+    protected float energiaDisponivel;
+    protected float energiaCompartilhada;
+    protected float energiaTotal;
 
     public SunShare(String nome, String email, String senha, String numeroTelefone, String endereco) {
         super(nome, email, senha, numeroTelefone, endereco);
@@ -37,19 +37,23 @@ public class SunShare extends Usuario {
     }
         
     
-    public void entradaDados () {//scanner
+    public void entradaDados () {
+        Scanner sc = new Scanner(System.in);
         System.out.println("/// Cadastro de novo item no Mercado de Energia ///");
         System.out.println("Aqui você cadastra todos os dados da sua geração de energia solar!");
-        System.out.println("Digite o total de energia que você gera: " + getEnergiaTotal());
-        System.out.println("Digite a energia que você já compartilha (se houver): " + getEnergiaCompartilhada());
-        System.out.println("Digite a energia disponivel para a venda: " + getEnergiaDisponivel());
+        System.out.println("Digite o total de energia que você gera: ");
+        setEnergiaTotal(energiaTotal = sc.nextFloat());
+        System.out.println("Digite a energia que você já compartilha (se houver): ");
+        setEnergiaCompartilhada(energiaCompartilhada = sc.nextFloat());
+        System.out.println("Digite a energia disponivel para a venda: ");
+        setEnergiaDisponivel(energiaDisponivel = sc.nextFloat());
     }
 
     public void imprimirShare () {
         System.out.println("/// Confira os dados cadastrados abaixo ///");
-        System.out.println("Energia total: " + energiaTotal);
-        System.out.println("Energia compartilhada: " + energiaCompartilhada);
-        System.out.println("Energia disponível: " + energiaDisponivel);
+        System.out.println("Energia total: " + getEnergiaTotal());
+        System.out.println("Energia compartilhada: " + getEnergiaCompartilhada());
+        System.out.println("Energia disponível: " + getEnergiaDisponivel());
     }
 
     public void verificarMercado() {
@@ -59,14 +63,21 @@ public class SunShare extends Usuario {
         System.out.println("Você está ciente de que esses dados serão enviados para o Mercado de Energia, deseja continuar? (S/N)");
 
         Scanner scanner = new Scanner(System.in);
-        String resposta = scanner.nextLine();
+        String resposta = scanner.nextLine(); //Criando a variável local para resposta do usuário
 
         if (resposta.equalsIgnoreCase("S")) {
             // Enviar os dados para o Mercado de Energia
             System.out.println("Dados enviados para o Mercado de Energia.");
+            ////////// Aqui você pode chamar um método que envia os dados para o Mercado de Energia/////////
         } else {
             System.out.println("Operação cancelada.");
         }
+    }
+
+    public static void main(String[] args) {
+        SunShare share1 = new SunShare();
+        share1.entradaDados();
+        share1.verificarMercado();
     }
 }
 
