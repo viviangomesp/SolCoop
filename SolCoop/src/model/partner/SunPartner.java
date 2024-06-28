7071f483e542d05f0f65cd1d89d209f147ac91b4
Gabriel Santos <GabrielDEV778@Outlook.com>
2024-06-27 21:57:32 -0300

    update sunpartner

--- SolCoop/src/model/partner/SunPartner.java
+++ SolCoop/src/model/partner/SunPartner.java
@@ -14,12 +14,12 @@ public class SunPartner extends Usuario {
         super(idUsuario, nome, email, senha, numeroTelefone, endereco);
         this.cep = cep;
         this.ultimasContas = new ArrayList<>();
-        this.empresasEscolhidas = new ArrayList<>();
+        this.empresaEscolhida = new ArrayList<>();
     }
 
     private String cep;
     private List<Float> ultimasContas;
-    private List<String> empresasEscolhidas;
+    private List<String> empresaEscolhida;
     private static Queue<SunPartner> filaDeEspera = new LinkedList<>();
     public float mediaConsumo = 0;
     public float margemErro = .15f; // 15% de margem de erro
@@ -41,12 +41,12 @@ public class SunPartner extends Usuario {
         this.ultimasContas = ultimasContas;
     }
 
-    public List<String> getEmpresasEscolhidas() {
-        return empresasEscolhidas;
+    public List<String> getEmpresaEscolhida() {
+        return empresaEscolhida;
     }
 
-    public void setEmpresasEscolhidas(List<String> empresasEscolhidas) {
-        this.empresasEscolhidas = empresasEscolhidas;
+    public void setEmpresaEscolhida(List<String> empresasEscolhidas) {
+        this.empresaEscolhida = empresasEscolhidas;
     }
 
     /* Método para calcular o consumo total de energia */
@@ -71,22 +71,26 @@ public class SunPartner extends Usuario {
         System.out.println("\nAqui você poderá solicitar um orçamento de energia solar e ficar a espera de um parceiro SolCoop!");
         System.out.println("Por favor, insira suas informações necessárias:");
         System.out.println("Insira o valor em kWh (quilowatt-hora) das suas últimas 12 contas de energia: ");
+        System.out.println();//Pular linha
 
         // Adiciona os valores das últimas contas de energia à lista
         for (int i = 0; i < 12; i++) {
-            System.out.print("Conta [" + (i + 1) + "]: ");
+            System.out.println("Conta [" + (i + 1) + "]: ");
             float conta = scanner.nextFloat();
             ultimasContas.add(conta);
         }
+
         PrintInfoUsuario(); // Chama o metodo de printar as infos do usuario
     }
 
     /* Metodo para printar as informações do usuário */
     public void PrintInfoUsuario() {
         float consumoFinal = ConsumoTotalPartner();
-        System.out.printf("\nSua média de consumo nas últimas 12 contas de energia: %.2f kWh", mediaConsumo);
-        System.out.printf("\nMédia com margem de segurança de 15%%: %.2f kWh", consumoFinal);
+        System.out.println("\n///////////////////////////////");
+        System.out.printf("Sua média de consumo nas últimas 12 contas de energia: [ %.2f kWh ]", mediaConsumo);
+        System.out.printf("\nMédia com margem de segurança de 15%%: [ %.2f kWh ]", consumoFinal);
         System.out.println("\nA margem de segurança é importante para que o seu consumo seja sempre atendido com energia limpa.");
+        System.out.println("///////////////////////////////");
 
         filaDeEspera.add(this); // Adiciona o usuário à fila
         escolhaEmpresa(this);
@@ -105,23 +109,23 @@ public class SunPartner extends Usuario {
 
             switch (escolhaDaEmpresa) {
                 case 1:
-                    sunPartner.getEmpresasEscolhidas().add("BRZ Solar");
+                    sunPartner.getEmpresaEscolhida().add("BRZ Solar");
                     System.out.println("Orçamento solicitado com sucesso para a empresa BRZ Solar!");
                     break;
                 case 2:
-                    sunPartner.getEmpresasEscolhidas().add("Inove Solar Engenharia");
+                    sunPartner.getEmpresaEscolhida().add("Inove Solar Engenharia");
                     System.out.println("Orçamento solicitado com sucesso para a empresa Inove Solar Engenharia!");
                     break;
                 case 3:
-                    sunPartner.getEmpresasEscolhidas().add("RenovaSol");
+                    sunPartner.getEmpresaEscolhida().add("RenovaSol");
                     System.out.println("Orçamento solicitado com sucesso para a empresa RenovaSol!");
                     break;
                 case 4:
-                    sunPartner.getEmpresasEscolhidas().add("DellaSol");
+                    sunPartner.getEmpresaEscolhida().add("DellaSol");
                     System.out.println("Orçamento solicitado com sucesso para a empresa DellaSol!");
                     break;
                 case 5:
-                    sunPartner.getEmpresasEscolhidas().add("Sun7");
+                    sunPartner.getEmpresaEscolhida().add("Sun7");
                     System.out.println("Orçamento solicitado com sucesso para a empresa Sun7!");
                     break;
                 default:
@@ -138,10 +142,10 @@ public class SunPartner extends Usuario {
             System.out.println("\nA fila de espera está vazia.");
             return;
         }
-        System.out.println("\nFila de Usuários:");
+        System.out.println("\n//////// Fila de Usuários: ////////");
         for (SunPartner sunPartner : filaDeEspera) {
-            String empresas = (sunPartner.getEmpresasEscolhidas() != null) ? sunPartner.getEmpresasEscolhidas().toString() : "Nenhuma empresa escolhida";
-            System.out.println("Nome: " + sunPartner.getNome() + " | CEP: " + sunPartner.getCep() + " | Empresas escolhidas: " + empresas);
+            String empresas = (sunPartner.getEmpresaEscolhida() != null) ? sunPartner.getEmpresaEscolhida().toString() : "Nenhuma empresa escolhida";
+            System.out.println("\nNome: " + sunPartner.getNome() + " | CEP: " + sunPartner.getCep() + " | Empresas escolhidas: " + empresas);
         }
     }
 
@@ -158,10 +162,12 @@ public class SunPartner extends Usuario {
     /* Método para solicitar orçamento */
     public void solicitarOrcamento() {
         float consumoFinal = ConsumoTotalPartner();
+        System.out.println();//Pular linha
+        System.out.println("//////////////////////////////////");
         System.out.println("/// Informações do orçamento: ///");
-        System.out.println("CEP: " + cep);
-        System.out.printf("Quantidade de energia desejada (com margem de segurança de 15%%) é %.2f kWh: \n", consumoFinal);
-        System.out.println("Empresas escolhidas: " + empresasEscolhidas);
+        System.out.println("\nCEP: " + cep);
+        System.out.printf("Quantidade de energia desejada (com margem de segurança de 15%%) é [ %.2f kWh ]: \n", consumoFinal);
+        System.out.println("Empresa escolhida: " + empresaEscolhida);
         System.out.println("\nVamos encontrar um parceiro SolCoop para você. Entraremos em contato em breve!\n");
     }
 
